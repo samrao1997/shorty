ffc2f86d98bd10c5c6e3185f4f84142a1fdfb421
Sam Rao <samrao1997@gmail.com>
2023-10-17 19:36:17 +0000

    Import redirects File

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
-
-const YAML = require('yaml')
-
-
 // Load all redirects from redirect.yml file
+const YAML = require('yaml')
+const fs = require('fs')
+const path = require('path')
+
+
+const redirectsFile = fs.readFileSync(path.join(__dirname, 'redirects.yml'), 'utf-8')
+console.log(redirectsFile)
+
 
 
 // Generate html page for each redirect url from template.html
